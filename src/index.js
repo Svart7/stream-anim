6f260b6f74b2a1366ac15f90c8233f618c5cd1b4
dmitry-vetrov-pro <dmitry.vetrov@ntprog.com>
2022-08-07 04:33:53 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -6,158 +6,44 @@ import loadedFont from "@fontsource/roboto/files/roboto-all-700-normal.woff";
 import sh_vert from './shaders/sh.vert'
 import sh_frag from './shaders/sh.frag'
 
-const totalParticles = 3;
-const maxRadius = 150;
-const minRadius = 50;
+const totalParticles = 30;
+const maxSpeed = 5;
+const lessMaxAccel = 40000;
+const maxRadius = 50;
+const minRadius = 2;
 const fontSize = 26;
-const canvasSideMulti = 0.90;
+const canvasSideMulti = 1;
+const showGrid = false;
+const cameraZ = 1500;
+const shaderCanvasSize = 256;
 let shader;
+let shaderTexture;
 
 /**
  * @param p {p5}
  */
 function sketch(p) {
   let particles = {};
-  let bgColor = chroma.rgb(0, 0, 0);
   let font;
   let midiEnabled = false;
   let fps = 60;
   let logAfter = Date.now() + 1000;
 
-  function horizontal(options, y) {
-    const { halfWidth } = options;
-    p.line(-1 * halfWidth, y, halfWidth, y);
-  }
-
-  function vertical(options, x) {
-    const { halfHeight } = options;
-    p.line(x,-1 * halfHeight, x, halfHeight);
-  }
-
-  function drawBg() {
-    const options = makeOptions();
-    p.background(bgColor.rgb());
-    printInfo(options)
-
-    p.stroke(20);
-    p.strokeWeight(1);
-
-    const {halfWidth, halfHeight} = options;
-    horizontal(options, -1 * halfHeight);
-    horizontal(options, halfHeight);
-    vertical(options, -1 * halfWidth);
-    vertical(options, halfWidth);
-
-    p.stroke(128, 128, 128, 0.25);
-    vertical(options, 0);
-    horizontal(options, 0);
-    return options;
-  }
-
-  function printInfo({ indexes }) {
-    p.textFont(font, fontSize);
-    p.fill(10, 150, 20);
-    p.textAlign(p.CENTER, p.TOP);
-
-    const currentFps = p.frameRate();
-    if (Math.abs(fps - currentFps) > 10)
-      fps = currentFps;
-
-    const fpsText = `${fps.toFixed(0)}fps`
-    const count = `${indexes.length} particles`;
-    let str = `${count} | ${fpsText}`
-    if (midiEnabled)
-      str = `MIDI | ${str}`;
-
-    p.text(str, 0, -0.5 * p.height)
-  }
-
-  function logState({ halfWidth, halfHeight } = {}) {
-    const objects = Object.values(particles);
-    let title = `full: ${p.width}x${p.height} `;
-
-    if (halfWidth && halfHeight)
-      title += `inner: ${halfWidth*2}x${halfHeight*2} `;
-
-    title += ` ${objects.length} particles`;
-    console.groupCollapsed(title);
-    objects.forEach((p) => {console.log(p.str())});
-    console.groupEnd()
-  }
-
-  function makeOptions() {
-    return {
-      halfWidth: Math.ceil(p.width * canvasSideMulti * 0.5),
-      halfHeight: Math.ceil(p.height * canvasSideMulti * 0.5),
-      indexes: Object.keys(particles).sort(),
-    }
-  }
-
-  function makeInitialCoord(maxCoord) {
-    let accel = 0;
-
-    while (accel === 0)
-      accel = p.random(-0.02, 0.02)
-
-    return {
-      coord: p.random(-1 * maxCoord, maxCoord),
-      speed: 0,
-      accel: accel,
-    }
-  }
-
-  function coordStep(particle, dimensionName, halfSide) {
-    const { r } = particle;
-    const dimension = particle[dimensionName];
-    let { speed, accel, coord } = dimension;
-    const sign = Math.sign(coord);
-    const maxCoord = halfSide - r - 2;
-    const overMax = Math.abs(coord + speed) - maxCoord;
-    const accelMaxDelta = maxCoord / 20000 * (r - minRadius);
-
-    if (Math.abs(speed) > 10) {
-      accel = -1 * Math.sign(accel) * p.random(accelMaxDelta);
-      console.debug(`${particle.str()} max speed reached starting decrease`);
-    }
-
-    if (Math.abs(speed) < 0.1) {
-      accel += Math.sign(accel) * p.random(accelMaxDelta);
-      console.debug(`${particle.str()} stopped starting increase`);
-    }
-
-    if(overMax > 0) {
-      let text = `${particle.str()} hitting ${dimensionName}. `
-      text += `with V=${speed.toFixed(1)}, a=${accel.toFixed(3)}. `
-      speed = speed * -1;
-
-      if (accel === 0) {
-        accel = -1 * sign * p.random(accelMaxDelta);
-      } else {
-        accel *= -1.05;
-      }
-
-      coord = sign * maxCoord;
-      particle.fixRadius()
-
-      console.debug(`${text} acceleration ${accel.toFixed(3)}`)
-    } else {
-      coord += speed;
-    }
-
-    dimension.speed = speed + accel;
-    dimension.coord = coord;
-    dimension.accel = accel;
-    return coord;
-  }
-
   class Particle {
     constructor(index, { halfWidth, halfHeight }) {
-      this.color = chroma.random();
+      let color = chroma.random();
+
+      while (chroma.contrast(color, "white") < 4.5) {
+        color = chroma.random();
+      }
+
+      this.color = color;
       this.index = index;
 
       const r = this.r = p.random(minRadius, maxRadius);
       this.x = makeInitialCoord(halfWidth - r);
       this.y = makeInitialCoord(halfHeight - r);
+      this.z = p.random(index * cameraZ / totalParticles)
       this.fixRadius()
     }
 
@@ -169,8 +55,52 @@ function sketch(p) {
       return Math.sqrt(halfWidth * halfWidth + halfHeight * halfHeight) / 2;
     }
 
+    coordStep(dimensionName, halfSide) {
+      const { r } = this;
+      const dimension = this[dimensionName];
+      let { speed, accel, coord } = dimension;
+      const sign = Math.sign(coord);
+      const maxCoord = halfSide - r - 2;
+      const overMax = Math.abs(coord + speed) - maxCoord;
+      const accelMaxDelta = maxCoord / lessMaxAccel * (r - minRadius);
+
+      if (Math.abs(speed) > maxSpeed) {
+        accel = -1 * Math.sign(accel) * p.random(accelMaxDelta);
+        console.debug(`${this.str()} max speed reached starting decrease`);
+      }
+
+      if (Math.abs(speed) < 0.1) {
+        accel += Math.sign(accel) * p.random(accelMaxDelta);
+        console.debug(`${this.str()} stopped starting increase`);
+      }
+
+      if(overMax > 0) {
+        let text = `${this.str()} hitting ${dimensionName}. `
+        text += `with V=${speed.toFixed(1)}, a=${accel.toFixed(3)}. `
+        speed = speed * -1;
+
+        if (accel === 0) {
+          accel = -1 * sign * p.random(accelMaxDelta);
+        } else {
+          accel *= -1.05;
+        }
+
+        coord = sign * maxCoord;
+        this.fixRadius()
+
+        console.debug(`${text} acceleration ${accel.toFixed(3)}`)
+      } else {
+        coord += speed;
+      }
+
+      dimension.speed = speed + accel;
+      dimension.coord = coord;
+      dimension.accel = accel;
+      return coord;
+    }
+
     step({ halfWidth, halfHeight, indexes }) {
-      let { color, r, fixRadiusUntil, x, y, index } = this;
+      let { color, r, fixRadiusUntil, x, y, z, index } = this;
       const touchDist = this.touchMaxDist({halfHeight, halfWidth});
 
       if (Date.now() > fixRadiusUntil && p.random() < 0.5) {
@@ -184,16 +114,11 @@ function sketch(p) {
         this.r = r;
       }
 
-      const xPos = coordStep(this, 'x', halfWidth);
-      const yPos = coordStep(this, 'y', halfHeight);
-
-      p.noStroke()
-      p.translate(xPos, yPos);
-      p.sphere(r);
-      p.translate(-1 * xPos, -1 * yPos);
+      const xPos = this.coordStep('x', halfWidth);
+      const yPos = this.coordStep('y', halfHeight);
 
       indexes.forEach((j) => {
-        const {x: x2, y: y2, color: color2} = particles[j];
+        const {x: x2, y: y2, z: z2, color: color2} = particles[j];
         if (j < index)
           return true;
 
@@ -211,31 +136,38 @@ function sketch(p) {
 
         p.stroke(...mixed.rgba());
         p.strokeWeight(alfa * 5);
-        p.line(xPos, yPos, x2Pos, y2Pos);
+        p.line(xPos, yPos, z, x2Pos, y2Pos, z2);
       });
+
+      p.noStroke()
+      p.fill(color.rgb())
+      p.translate(xPos, yPos, z);
+      p.sphere(r);
+      p.translate(-1 * xPos, -1 * yPos, -z);
+      p.noFill()
     }
 
     str() {
-      const { x, y } = this;
-      const coords = `(${x.coord.toFixed(0)};${y.coord.toFixed(0)})`;
+      const { x, y, z, color } = this;
+      const coords = `(${x.coord.toFixed(0)};${y.coord.toFixed(0)};${z.toFixed(0)})`;
       const speed = `(${x.speed.toFixed(1)};${y.speed.toFixed(1)})`;
       const accel = `(${x.accel.toFixed(3)};${y.accel.toFixed(3)})`;
-      return `${this.index} ${coords} V=${speed} a=${accel}`
+      return `${this.index} ${color.hex()} ${coords} V=${speed} a=${accel}`
     }
-  }
-
-  p.preload = function () {
-    font = p.loadFont(loadedFont)
-    // shader = p.loadShader('shaders/sh.vert', 'shaders/sh.frag');
-    shader = p.loadShader(sh_vert, sh_frag);
   }
 
   p.windowResized = function () {
     p.resizeCanvas(p.windowWidth, p.windowHeight);
   }
 
+  p.preload = function () {
+    font = p.loadFont(loadedFont)
+    shader = p.loadShader(sh_vert, sh_frag);
+  }
+
   p.setup = function () {
     const enable = WebMidi.enable();
+
     enable.then(() => {
       let inFiltered = WebMidi.inputs.filter(({ name }) => name.indexOf('MkIII') !== -1);
       inFiltered = inFiltered.filter(({ name }) => name.indexOf('MIDI') !== -1);
@@ -265,28 +197,126 @@ function sketch(p) {
 
     enable.catch(err => console.error(err))
     const { innerWidth, innerHeight } = window;
+
     p.createCanvas(innerWidth, innerHeight, p.WEBGL);
     p.colorMode(p.RGB);
+    p.noStroke()
 
-    p.shader(shader)
+    shaderTexture = p.createGraphics(shaderCanvasSize, shaderCanvasSize, p.WEBGL);
+    shaderTexture.noStroke();
+
     const options = makeOptions();
     for(let i = 0; i < totalParticles; i++){
       particles[i] = new Particle(i, options);
     }
 
+    const { halfWidth, halfHeight } = options;
+    p.ortho(-halfWidth, halfWidth, -halfHeight, halfHeight, 0, cameraZ);
     logState();
   };
 
   p.draw = function () {
     const options = drawBg();
-    options.indexes.forEach(i => particles[i].step(options));
 
+    options.indexes.forEach(i => particles[i].step(options));
     if (Date.now() > logAfter) {
       logState(options);
       logAfter = Date.now() + 5000;
     }
+  }
 
-    p.pointLight("white", 0, -options.halfHeight, 0);
+    function horizontal(options, y) {
+    const { halfWidth } = options;
+    p.line(-1 * halfWidth, y, halfWidth, y);
+  }
+
+  function vertical(options, x) {
+    const { halfHeight } = options;
+    p.line(x,-1 * halfHeight, x, halfHeight);
+  }
+
+  function drawBg() {
+    const options = makeOptions();
+    const { halfWidth, halfHeight } = options;
+
+    shaderTexture.shader(shader)
+    shader.setUniform("time", p.millis() / 1000.0);
+
+    shaderTexture.rect(-shaderCanvasSize / 2, -shaderCanvasSize / 2, shaderCanvasSize, shaderCanvasSize);
+    p.background(255);
+    p.push()
+    p.texture(shaderTexture)
+    p.textureWrap(p.MIRROR)
+    p.rect(-halfWidth, -halfHeight, halfWidth * 2, halfHeight * 2)
+
+    printInfo(options)
+    if (showGrid) {
+      p.stroke(20);
+      p.strokeWeight(1);
+
+      horizontal(options, -1 * halfHeight);
+      horizontal(options, halfHeight);
+      vertical(options, -1 * halfWidth);
+      vertical(options, halfWidth);
+
+      p.stroke(128, 128, 128, 0.25);
+      vertical(options, 0);
+      horizontal(options, 0);
+    }
+
+    printInfo(options);
+    return options;
+  }
+
+  function printInfo({ indexes }) {
+    p.textFont(font, fontSize);
+    p.fill("blue");
+    p.textAlign(p.CENTER, p.TOP);
+
+    const currentFps = Math.round(p.frameRate());
+    if (Math.abs(fps - currentFps) > 30 || (p.frameCount % currentFps) === 0)
+      fps = currentFps;
+
+    const count = `${indexes.length} particles`;
+    let str = `${count} | ${fps}fps`
+    if (midiEnabled)
+      str = `MIDI | ${str}`;
+
+    p.text(str, 0, -0.5 * p.height)
+  }
+
+  function logState({ halfWidth, halfHeight } = {}) {
+    const objects = Object.values(particles);
+    let title = `full: ${p.width}x${p.height} `;
+
+    if (halfWidth && halfHeight)
+      title += `inner: ${halfWidth*2}x${halfHeight*2} `;
+
+    title += ` ${objects.length} particles`;
+    console.groupCollapsed(title);
+    objects.forEach((p) => {console.log(p.str())});
+    console.groupEnd()
+  }
+
+  function makeOptions() {
+    return {
+      halfWidth: Math.round(p.width * canvasSideMulti * 0.5),
+      halfHeight: Math.round(p.height * canvasSideMulti * 0.5),
+      indexes: Object.keys(particles).sort(),
+    }
+  }
+
+  function makeInitialCoord(maxCoord) {
+    let accel = 0;
+
+    while (accel === 0)
+      accel = p.random(-0.02, 0.02)
+
+    return {
+      coord: p.random(-1 * maxCoord, maxCoord),
+      speed: 0,
+      accel: accel,
+    }
   }
 }
 
--- src/shaders/sh.frag
+++ src/shaders/sh.frag
@@ -4,12 +4,12 @@ precision mediump float;
 
 uniform vec2 u_resolution;
 uniform vec2 u_mouse;
-uniform float u_time;
+uniform float time;
 
 varying vec2 vTexCoord;
 
 vec2 random2( vec2 p ) {
-    return fract(sin(vec2(dot(p,vec2(127.1,311.7)),dot(p,vec2(269.5,183.3))))*43758.5453);
+    return fract(sin(vec2(dot(p, vec2(127.1, 311.7)), dot(p, vec2(269.5,183.3)))) * 43758.5453);
 }
 
 vec3 voronoi( in vec2 x ) {
@@ -23,7 +23,7 @@ vec3 voronoi( in vec2 x ) {
         for (int i= -1; i <= 1; i++) {
             vec2 g = vec2(float(i),float(j));
             vec2 o = random2( n + g );
-            o = 0.5 + 0.5*sin( u_time + 6.2831*o );
+            o = 0.5 + 0.5*sin( time + 6.2831*o );
 
             vec2 r = g + o - f;
             float d = dot(r,r);
@@ -40,17 +40,18 @@ vec3 voronoi( in vec2 x ) {
     md = 8.0;
     for (int j= -2; j <= 2; j++) {
         for (int i= -2; i <= 2; i++) {
-            vec2 g = mg + vec2(float(i),float(j));
+            vec2 g = mg + vec2(float(i), float(j));
             vec2 o = random2( n + g );
-            o = 0.5 + 0.5*sin( u_time + 6.2831*o );
+            o = 0.5 + 0.5*sin( time + 6.2831 * o );
 
             vec2 r = g + o - f;
 
-            if ( dot(mr-r,mr-r)>0.00001 ) {
+            if ( dot(mr-r,mr-r) > 0.00001 ) {
                 md = min(md, dot( 0.5*(mr+r), normalize(r-mr) ));
             }
         }
     }
+
     return vec3(md, mr);
 }
 
@@ -59,16 +60,16 @@ void main() {
     vec3 color = vec3(0.);
 
     // Scale
-    st *= 3.;
+    st *= 6. ;
     vec3 c = voronoi(st);
 
     // isolines
-    color = c.x*(0.5 + 0.5*sin(64.0*c.x))*vec3(1.0);
+    color = c.x*(0.5 + 0.5*sin(64.0*c.x)) * vec3(1.0);
     // borders
     color = mix( vec3(1.0), color, smoothstep( 0.01, 0.02, c.x ) );
     // feature points
     float dd = length( c.yz );
-    color += vec3(1.)*(1.0-smoothstep( 0.0, 0.04, dd));
+    color += vec3(1.) * (1.0-smoothstep( 0.0, 0.04, dd));
 
-    gl_FragColor = vec4(color,1.0);
+    gl_FragColor = vec4(color, 1.0);
 }
